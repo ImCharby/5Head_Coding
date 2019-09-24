7efe0ca1783ab4bc2d42c4e4e46667e4cf4b83bb
Tommy <thomas.p576@stu.nebo.edu>
2019-09-24 10:28:59 -0600

    Finished Challenge 1

--- Create with Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Create with Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -7,6 +7,12 @@ public class FollowPlayerX : MonoBehaviour
     public GameObject plane;
     public Vector3 offset;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-<<<<<<< HEAD
     //player control private variables
     private float speed = 20f;
     private float turnSpeed = 50f;
     private float horizontalInput;
     private float forwardInput;
-=======
-    public float speed = 20f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
->>>>>>> master
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         //help move and rotate vehicle
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-=======
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+
         //We'll move the vehicle forward
->>>>>>> master
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
