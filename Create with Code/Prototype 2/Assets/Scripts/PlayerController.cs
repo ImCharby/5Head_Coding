acc1e7ca2da855cee48383b52540ae2c3fca22da
Tommy <thomas.p576@stu.nebo.edu>
2019-10-10 10:27:54 -0600

    Added Projectile

--- Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 15f;
     public float xRange = 10f;
+
+    public GameObject projectilePrefab;
     
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,9 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+
     {
+
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -29,6 +33,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
-        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
