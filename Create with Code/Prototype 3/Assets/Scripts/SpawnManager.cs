439d09781acdf8cda9ebaa70303c5e0ea73a2367
Tommy <thomas.p576@stu.nebo.edu>
2019-11-07 10:27:31 -0700

    Human Helix

--- Create with Code/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Create with Code/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 0;
-    private float repeatRate = .0001f;
+    private float repeatRate = 2f;
 
     // Start is called before the first frame update
     void Start()
