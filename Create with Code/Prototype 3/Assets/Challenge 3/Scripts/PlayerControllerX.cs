217052ca8323cc0759f038bce9afa3b5a87c9e34
Tommy <thomas.p576@stu.nebo.edu>
2019-11-15 10:29:34 -0700

    Finished Challenge 3

--- Create with Code/Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Create with Code/Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -16,38 +16,37 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip boingSound;
+
 
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
 
         // Apply a small upward force at the start of the game
-        playerRb = GetComponent<Rigidbody>();
-
-    }
+        playerRb = GetComponent<Rigidbody>(); }
 
     // Update is called once per frame
     void Update()
     {
         // While space is pressed and player is low enough, float up
         if (Input.GetKey(KeyCode.Space) && !gameOver) {
-            playerRb.AddForce(Vector3.up * floatForce); }
-    }
+            playerRb.AddForce(Vector3.up * floatForce); } }
 
+
+    
     private void OnCollisionEnter(Collision other)
     {
         // if player collides with bomb, explode and set gameOver to true
-        if (other.gameObject.CompareTag("Bomb"))
-        {
+        if (other.gameObject.CompareTag("Bomb")) {
             explosionParticle.Play();
             playerAudio.PlayOneShot(explodeSound, 1.0f);
             gameOver = true;
             Debug.Log("Game Over!");
-            Destroy(other.gameObject);
-        } 
+            Destroy(other.gameObject); }
+
 
         // if player collides with money, fireworks
         else if (other.gameObject.CompareTag("Money"))
@@ -58,6 +57,11 @@ public class PlayerControllerX : MonoBehaviour
 
         }
 
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            playerRb.AddForce(Vector3.up * floatForce * 15);
+            playerAudio.PlayOneShot(boingSound, 10.0f);
+        }
     }
 
 }
